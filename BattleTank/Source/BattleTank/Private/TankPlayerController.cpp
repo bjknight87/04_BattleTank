eae8ba59e1f2365cd0c12e2ab4fcf2c1cefe1355
bjknight87 <bjknight87@yahoo.com>
2018-12-02 15:32:04 -0600

    Added ability for AIController to locate player tank.

--- BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -3,6 +3,21 @@
 //#include "BattleTank.h"
 #include "../Public/TankPlayerController.h"
 
+
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing tank."));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing tank %s."), *(ControlledTank->GetName()));
+	}
+	
+}
+
 ATank* ATankPlayerController::GetControlledTank() const 
 {
 	return Cast<ATank>(GetPawn());
